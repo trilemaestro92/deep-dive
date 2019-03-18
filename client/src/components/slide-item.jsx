dc653193c7851b6a88c82a1ff1b433717e835179
Tri Nguyen <trihuynguyen14@gmail.com>
2019-03-18 15:27:58 -0400

    styling

--- client/src/components/slide-item.jsx
+++ client/src/components/slide-item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,16 +10,20 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+
 const styles = {
   card: {
     maxWidth: 345,
+    backgroundColor:'#282828'
   },
   media: {
-    height: 300,
+    height: 290,
     backgroundSize: 'cover',
     backgroundPosition: 'center'
   },
-  
+  textBody:{
+      backgroundColor: '#282828'
+  }
 };
 
 function MediaCard(props) {
@@ -31,23 +36,24 @@ function MediaCard(props) {
           image={props.image}
           title={props.title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+        <CardContent className={classes.textBody} >
+          <Typography gutterBottom variant='body1' color='secondary'  component="h2" align='left'>
             {props.header}
           </Typography>
-          <Typography component="p">
-            {props.body}
+          <Typography variant='subtitle2' color='textSecondary' component="p">
+            {/* {props.body} */}
+            2019 • 
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      {/* <CardActions className={classes.textBody}>
         <Button size="small" color="primary">
           Share
         </Button>
         <Button size="small" color="primary">
           Learn More
         </Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
